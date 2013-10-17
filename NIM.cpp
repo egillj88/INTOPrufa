637e04908a9367e69c3591b3adcf3afdcb25ca8d
ragnalara <ragnalara@gmail.com>
2013-10-17 11:48:37 +0000

    Update NIM.cpp

--- NIM.cpp
+++ NIM.cpp
@@ -105,7 +105,7 @@ void hjalp( )
 	cout << "einni hrugu i einu.  Sa sem tekur sidustu eldspytuna " << endl;
 	cout << "ur sidustu hrugunni vinnur.  Gangi ykkur vel! " << endl << endl;
 	cout << "Sladu a einhvern lykil... " << endl;
-	cout << "Egill Jónsson"
+	cout << "Egill Jónsson og Ragna Ellertsdottir"
 	getchar( );
 }
 
